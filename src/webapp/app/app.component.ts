dc8bc5b4b18f37f0fdb6412ff92c68ebcc6714a1
m11t <melfthomsen@gmail.com>
2017-01-25 20:47:21 +0100

    Login form with JWT authentication and success handling

--- src/webapp/app/app.component.ts
+++ src/webapp/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { User } from './classes/user';
 
+/**
+ * Application Component
+ * 
+ * @export
+ * @class AppComponent
+ */
 @Component({
   selector: 'paf-quiz',
   templateUrl: 'app/app.component.html'
@@ -11,7 +17,17 @@ export class AppComponent {
 
     constructor() {
         this.user = new User();
-        this.user.logout();
+    }
+
+    /**
+     * Caches the user data after successful login
+     * 
+     * @param {User} user
+     * 
+     * @memberOf AppComponent
+     */
+    public onLoginSuccessful(user: User) {
+        this.user = user;
     }
 
 }
--- src/webapp/app/login.component.ts
+++ src/webapp/app/login.component.ts
@@ -1,13 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './classes/user';
 
+/**
+ * Login Component 
+ * 
+ * @export
+ * @class LoginComponent
+ */
 @Component({
   selector: 'login',
-  templateUrl: 'app/login.component.html'
+  templateUrl: 'app/login.component.html',
+  providers: [
+    UserService
+  ]
 })
 export class LoginComponent {
 
-    @Input()
-    user : User;
+    user: User;
+
+    @Output()
+    onLoginSuccessful = new EventEmitter();
+
+    constructor(public userService: UserService) { 
+        this.user = new User();
+    }
+
+    /**
+     * Tries a login with the current user credentials entered into the form.
+     * 
+     * 
+     * @memberOf LoginComponent
+     */
+    public onSubmit() {
+        this.userService.login(this.user.id, this.user.password)
+          .subscribe(user => this.onLoginSuccessful.emit(user));
+    }
     
 }
