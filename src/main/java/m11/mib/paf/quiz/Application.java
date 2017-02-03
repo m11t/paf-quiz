a0698103727b9252672c8ad9905956463cc79806
m11t <melfthomsen@gmail.com>
2017-02-03 18:23:50 +0100

    Force Spring to let routing be done by angular2 for unknown resources

--- src/main/java/m11/mib/paf/quiz/Application.java
+++ src/main/java/m11/mib/paf/quiz/Application.java
@@ -1,12 +1,21 @@
 package m11.mib.paf.quiz;
 
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
 import m11.mib.paf.quiz.user.User;
 import m11.mib.paf.quiz.user.UserRepository;
 
@@ -28,6 +37,22 @@ public class Application {
     public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
     }
+
+    /**
+     * Redirect all unsupported requests to the angular app
+     * @return 
+     */
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return new ErrorViewResolver() {
+            @Override
+            public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+                return status == HttpStatus.NOT_FOUND
+                        ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+                        : null;
+            }
+        };
+    }    
     
     @Bean
     public CommandLineRunner demo(
